278da2377337adf4bb4535e1a72de6a41381f5ad
ctc5367 <ctc5367@psu.edu>
2020-09-10 15:49:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Author: Cole Carter ctc5367@psu.edu
+# Collaborator: Mark Del Grande mxd5728@psu.edu
+# Collaborator: Zak Young zjy5116@psu.edu
+# Collaborator: Ethan Moyer epm5482@psu.edu
+# Section: 5
+# Breakout: 4
+
+def getLetterGrade(grade):
+  if grade >= 93.0:
+    print("Your letter grade for CMPSC 131 is A.")
+  elif grade >= 90.0:
+    print("Your letter grade for CMPSC 131 is A-.")
+  elif grade >= 87.0:
+    print("Your letter grade for CMPSC 131 is B+.")
+  elif grade >= 83.0:
+    print("Your letter grade for CMPSC 131 is B.")
+  elif grade >= 80.0:
+    print("Your letter grade for CMPSC 131 is B-.")
+  elif grade >= 77.0:
+    print("Your letter grade for CMPSC 131 is C+.")
+  elif grade >= 70.0:
+    print("Your letter grade for CMPSC 131 is C.")
+  elif grade >= 60.0:
+    print("Your letter grade for CMPSC 131 is D.")
+  else:
+    print("Your letter grade for CMPSC 131 is F.")
+
+def run():
+  grade = input("Enter your CMPSC 131 grade:")
+  getLetterGrade(float(grade))
+
+if __name__ ==  "__main__":
+  run()
